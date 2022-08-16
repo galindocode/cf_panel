63716a2ca445d717474154fdd7e4d60dc87a7ae1
Leonardo Galindo <galindocode@gmail.com>
2022-08-15 20:45:01 -0400

    change in the seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,12 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        /* \App\Models\User::factory(10)->create(); */
         \App\Models\User::factory()->create([
             'email' => "admin@gmail.com",
             'password' => bcrypt('1234567890'),
         ]);
-        \App\Models\Categories::factory(10)->create();
-        \App\Models\Videos::factory(40)->create();
+        /* \App\Models\Categories::factory(10)->create();
+        \App\Models\Videos::factory(40)->create(); */
     }
 }
